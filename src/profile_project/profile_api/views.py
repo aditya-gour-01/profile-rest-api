2674782a6610509fe9febcc87e898546fcf31bcb
unknown <adityagour2812@gmail.com>
2023-03-20 23:05:59 +0530

    search function to our user profile api

--- src/profile_project/profile_api/views.py
+++ src/profile_project/profile_api/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
+
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from . import serializers
-from . import models
+from rest_framework.authentication import TokenAuthentication
 from rest_framework import status
+from rest_framework import filters
 
 from . import permission
-from rest_framework.authentication import TokenAuthentication
+from . import serializers
+from . import models
+
 # Create your views here.
 class HelloApiView(APIView):
     """Test Api View"""
@@ -97,3 +100,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset=models.UserProfile.objects.all()
     authentication_classes=(TokenAuthentication,)
     permission_classes=(permission.UpdateOwnProfile,)
+    filter_backends=(filters.SearchFilter,)
+    search_fields=('name','email',)
+    
